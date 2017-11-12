34a29403371b88cfc1eeba22298178ec2f6a39ab
Jon  McClennon <jonathan.mcclennon@sky.com>
2017-11-12 17:37:03 +0000

    Home page api call

--- basic-app/src/Home.js
+++ basic-app/src/Home.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 class Home extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  async componentDidMount() {
+    const results = await axios('//localhost:3001/api/ppvs');
+    this.setState({ results: results.data.body, isLoading: false })
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <div>Fetching ppvs</div>
+    }
+    const ppvs = this.state.results.map((result) => {
+      return <li key={result.id} className="ppv__event"><Link to={`/ppv/${result.id}`}>{result.name}</Link></li>
+    });
+
     return <ul>
-      <li className="ppv__event"><Link to="/ppv/ss">Surviour Seriers</Link></li>
-      <li className="ppv__event">TLC</li>
-      <li className="ppv__event">Hell in a Cell</li>
-      <li className="ppv__event">No Mercy</li>
-      <li className="ppv__event">Summerslam</li>
+      {ppvs}
     </ul>
   }
 }
