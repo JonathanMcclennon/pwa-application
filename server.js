a8e2dc2bc326d35e3fb2329a970463a96fa85d48
Jon  McClennon <jon.mcclennon@gmail.com>
2017-11-17 14:11:24 +0000

    Adding http2 server push

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const spdy = require('spdy')
+const path = require('path')
+const fs = require('fs')
+const https = require('https');
 const app = express()
+const port = process.env.PORT || 3001;
+const options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert: fs.readFileSync(__dirname + '/server.crt')
+}
 
 app.use('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -27,6 +36,37 @@ app.get('/api/ppvs/:id', (req, res) => {
 });
 
 app.get('/pwa-app', (req, res) => {
+    if (process.env.HTTP2) {
+    const js = fs.readFileSync(__dirname + '/pwa-app/build/static/js/main.5b626f24.js')
+    const css = fs.readFileSync(__dirname + '/pwa-app/build/static/css/main.58e36bd4.css');
+    var stream = res.push('/static/js/main.5b626f24.js', {
+        status: 200, // optional 
+        method: 'GET', // optional 
+        request: {
+            accept: '*/*'
+        },
+        response: {
+            'content-type': 'application/javascript'
+        }
+    });
+    stream.on('error', function () {
+    });
+    stream.end(js);
+
+     var cssStream = res.push('/static/css/main.58e36bd4.css', {
+        status: 200, // optional 
+        method: 'GET', // optional 
+        request: {
+            accept: '*/*'
+        },
+        response: {
+            'content-type': 'text/css'
+        }
+    });
+    cssStream.on('error', function () {
+    });
+    cssStream.end(css);
+    }
     res.sendFile(__dirname + '/pwa-app/build/index.html')
 });
 
@@ -34,4 +74,17 @@ app.get('/pwa-app/*', (req, res) => {
     res.sendFile(__dirname + '/pwa-app/build/index.html')
 })
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+//const server = https.createServer(options, app);
+//server.listen(3001);
+//app.listen(3001, () =>console.log('Example app listening on port 3001!'))
+
+spdy
+    .createServer(options, app)
+    .listen(port, (error) => {
+        if (error) {
+            console.error(error)
+            return process.exit(1)
+        } else {
+            console.log('Listening on port: ' + port + '.')
+        }
+    });
